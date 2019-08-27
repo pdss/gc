98196f79a47e4e4501543beca0d62c4a2f603c9e
luotao <307958201@qq.com>
2019-08-27 09:42:22 +0800

    lt

---
+++ src/main/java/com/zzl/gc/DTO/WechatPayAccountModel.java
@@ -0,0 +1,44 @@
+package com.zzl.gc.DTO;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author luotao
+ * @Package com.zzl.gc.config
+ * @date 2019/8/27 9:37
+ */
+@Data
+@Component
+public class WechatPayAccountModel {
+    /**
+     * 公众账号ID
+     */
+    @Value("${wxapp.appId}")
+    private String appId;
+
+    /**
+     * 商户号
+     */
+    @Value("${wxapp.mchId}")
+    private String mchId;
+
+    /**
+     * 商户密钥
+     */
+    @Value("${wxapp.mchKey}")
+    private String mchKey;
+
+    /**
+     * 商户证书路径
+     */
+    @Value("${wxapp.keyPath}")
+    private String keyPath;
+
+    /**
+     * 微信支付异步通知地址
+     */
+    @Value("${wxapp.notifyUrl}")
+    private String notifyUrl;
+}
